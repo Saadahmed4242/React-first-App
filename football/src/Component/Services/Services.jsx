2b5b01e7243abbbde318bfd53265ea624cf01cca
[Saadahmed4242] <[saadak671@gmail.com]>
2024-08-03 21:50:02 +0500

    Service file change

--- football/src/Component/Services/Services.jsx
+++ football/src/Component/Services/Services.jsx
@@ -18,14 +18,14 @@ export default function Services() {
             <div className="card ser" >
             <img src={service2} className="card-img-top ser-img" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">Service-1</h5>
+                <h5 className="card-title">Service-2</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
             </div>
             <div className="card ser" >
             <img src={service2} className="card-img-top ser-img" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">Service-1</h5>
+                <h5 className="card-title">Service-3</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
             </div>
